9a6f6eee2ee06a9b60c954a2ffd9b66021b85801
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2022-12-25 19:10:03 -0300

    Altera cores do fundo

--- js/main.js
+++ js/main.js
@@ -6,19 +6,19 @@ const rightButton = document.querySelector("#rightButton")
 
 // Funções
 
-const insertIntoHtml = (pokemons = []) =>{
+const insertIntoHtml = (pokemons = []) => {
     const pokeList = document.getElementById("pokeList")
     pokeList.innerHTML = pokemons.join("")
 }
 
 // Eventos
 
-rightButton.addEventListener("click", _ =>{
+rightButton.addEventListener("click", _ => {
     actuallyOffset += 20;
     pokeApi.getPokemons(url(actuallyOffset)).then(insertIntoHtml)
 })
 
-leftButton.addEventListener("click", _ =>{
+leftButton.addEventListener("click", _ => {
     actuallyOffset = actuallyOffset == 0 ? 0 : actuallyOffset -= 20;
     pokeApi.getPokemons(url(actuallyOffset)).then(insertIntoHtml)
 })
--- js/pokeApi.js
+++ js/pokeApi.js
@@ -1,25 +1,29 @@
 const pokeApi = {}
 
 pokeApi.pokemonColor = {
-    normal : "#A8A77A",
-    fire : "#EE8130",
-    water : "#6390F0",
-    electric : "#F7D02C",
-    grass : "#7AC74C",
-    ice : "#96D9D6",
-    fighting : "#C22E28",
-    poison : "#A33EA1",
-    ground : "#E2BF65",
-    flying : "#A98FF3",
-    psychic : "#F95587",
-    bug : "#A6B91A",
-    rock : "#B6A136",
-    ghost : "#735797",
-    dragon : "#6F35FC",
-    dark : "#705746",
-    steel : "#B7B7CE",
-    fairy : "#D685AD",
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
 }
+
+const btnEnviar = document.querySelector(".btn-enviar")
+
+
 
 pokeApi.getPokemonDetails = (pokemon) => {
     return fetch(pokemon.url)
@@ -28,8 +32,8 @@ pokeApi.getPokemonDetails = (pokemon) => {
 }
 
 pokeApi.convertPokemonJsonToHtml = (pokeInfo) => {
-    const [{type}] = pokeInfo.types
-    
+    const [{ type }] = pokeInfo.types
+        
     return `
         <li class="pokeCard" style="background-color:${pokeApi.pokemonColor[type.name]}">
         <div class="pokemonContrast">
@@ -40,10 +44,11 @@ pokeApi.convertPokemonJsonToHtml = (pokeInfo) => {
             <span class="pokeType">${type.name}</span>
         </li>
     `
+
 }
 
 pokeApi.getPokemons = (url) => {
-   return fetch(url)
+    return fetch(url)
         .then(response => response.json())
         .then(jsonBody => jsonBody.results)
         .then(pokemonList => pokemonList.map(pokeApi.getPokemonDetails))
